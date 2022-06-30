ab89dddc3c24c4edc76fbd453f254c92d3c5afbb
Pamela <pamelaeugeniamorales@gmail.com>
2022-06-30 10:56:09 -0300

    cambios en hard & soft skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortfolioService } from './servicios/portfolio.service';
 import { HttpClientModule} from '@angular/common/http';
+import { HysComponent } from './components/hys/hys.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +21,9 @@ import { HttpClientModule} from '@angular/common/http';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HabilidadesComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
